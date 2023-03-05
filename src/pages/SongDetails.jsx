79c576c20a13c0a7b30efef8a46fb5305fab2c7d
Eric Jiang <eyjiang18@gmail.com>
2023-03-04 22:55:57 -0500

    pulled song data to display lyrics

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -3,13 +3,13 @@ import { useSelector, useDispatch } from 'react-redux';
 import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
 
 import { setActiveSong, playPause } from '../redux/features/playerSlice';
+import { useGetSongDetailsQuery } from '../redux/services/shazamCore';
 
 const SongDetails = () => {
   const dispatch = useDispatch();
   const { songid } = useParams();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
-
-  console.log(songid);
+  const { data: songData, isFetching: isFetchingSongDetails } = useGetSongDetailsQuery({ songid });
 
   return (
     <div className="flex flex-col">
@@ -19,7 +19,14 @@ const SongDetails = () => {
           Lyrics:
         </h2>
         <div className="mt-5">
-          <p></p>
+          {songData?.sections[1].type === 'LYRICS' ? songData?.sections[1].text.map((line, i) => (
+            <p
+              key={`lyrics-${line}-${i}`}
+              className="text-gray-400 text-base my-1"
+            >
+              {line}
+            </p>
+          )) : (<p className="text-gray-400 text-base my-1">Lyrics not available</p>)}
         </div>
       </div>
     </div>
