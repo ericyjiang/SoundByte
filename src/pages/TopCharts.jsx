f20bee1145d440e56a752ce54ba4796a9e4504ad
Eric Jiang <eyjiang18@gmail.com>
2023-03-05 14:02:02 -0500

    added top chart component

--- src/pages/TopCharts.jsx
+++ src/pages/TopCharts.jsx
@@ -1,6 +1,4 @@
-import axios from 'axios';
 import { useSelector } from 'react-redux';
-
 import { Error, Loader, SongCard } from '../components';
 
 import { useGetTopChartsQuery } from '../redux/services/shazamCore';
