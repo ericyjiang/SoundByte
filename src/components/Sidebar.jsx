483040a8a51d3d4d86c941302dac47513f857c41
Eric Jiang <eyjiang18@gmail.com>
2023-03-06 17:38:00 -0500

    updated logo on web and mobile

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -28,8 +28,8 @@ const Sidebar = () => {
 
   return (
     <>
-      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
-        <img src={logo} alt="logo" className="w-full h-28 -mb-10 object-contain" />
+      <div className="md:flex hidden flex-col w-[240px] py-5 px-4 bg-[#191624]">
+        <img src={logo} alt="logo" className="w-full h-29 -mb-14 object-contain" />
         <NavLinks />
       </div>
 
@@ -38,12 +38,11 @@ const Sidebar = () => {
           <RiCloseLine className="w-6 h-6 text-white mr-2" onClick={() => setMobileMenuOpen(false)} />) : <HiOutlineMenu className="w-6 h-6 text-white mr-2" onClick={() => setMobileMenuOpen(true)} />}
       </div>
       <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483d8b] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
-        <img src={logo} alt="logo" className="w-full h-12 object-contain" />
+        <img src={logo} alt="logo" className="w-full h-13 -mb-10 object-contain" />
         <NavLinks
           handleClick={() => setMobileMenuOpen(false)}
         />
       </div>
-
     </>
   );
 };
